bb106390fe55be30ce42286e04e9fc633e15e3d1
Jens Nothnagel <jens@jensnotagelsair.speedport.ip>
2019-10-29 22:37:06 +0100

    led test

---
+++ ledTest.lua
@@ -0,0 +1,47 @@
+
+local maxLed = 118
+local ledOut = {}
+
+local ledWords = {
+    ES = { 0, 1 },
+    IST = { 3, 4, 5, },
+    FUENF_M = { 7, 8, 9, 10 }
+}
+
+local ledColor = {
+    RED = {255,0,0},
+    GREEN = {0,255,0},
+    BLUE = {0,0,255},
+    BLACK ={0,0,0}
+}
+
+local ledBrightness = {
+    LOW = 5,
+    MEDIUM = 15,
+    HIGH = 31
+}
+
+--[
+-- reset alls leds
+for i=1,maxLed do
+    ledOut[i]={0,table.unpack(ledColor.BLACK)}
+end
+--]]
+
+for k,v in pairs(ledWords) do
+    print(k,v)
+    for l,m in ipairs(v) do
+        print(l,m)
+        ledOut[m]={ledBrightness.MEDIUM, table.unpack(ledColor.BLUE)}
+    end
+end
+
+print(ledBrightness.LOW, table.unpack(ledColor.GREEN))
+
+temp={}
+for k,v in ipairs(ledOut) do
+    temp=table.concat(v)
+end
+print(string.char(table.unpack(ledOut)))
+
+
--- tables.lua
+++ tables.lua
@@ -1,4 +1,5 @@
 -- table datentyp
+--[[
 local t = {} -- leere tabelle
 
 table.insert( t, 'Karin' )
@@ -20,3 +21,35 @@ function pubHandler(client, t)
 end
 
 pubHandler("mqClient", {key1=1.23, key2="feed", key3, key4, key5})
+
+--]]
+
+
+local ledOut = {}
+
+local ledWords = {
+    ES = { 0, 1 },
+    IST = { 3, 4, 5, },
+    FUENF_M = { 7, 8, 9, 10 }
+}
+
+local ledColor = {
+    RED = {255,0,0},
+    GREEN = {0,255,0},
+    BLUE = {0,0,255}
+}
+
+local ledBrightness = {
+    LOW = 5,
+    MEDIUM = 15,
+    HIGH = 31
+}
+
+
+for k,v in pairs(mt) do
+    print(k,v)
+    for l,m in ipairs(v) do
+        print(l,m)
+    end
+end
+
